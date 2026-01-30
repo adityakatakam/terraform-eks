7f0de3092760a837f9469094a3bc8749842fd64f
akatakam <aditya.katakam@gmail.com>
2026-01-30 18:54:39 +0530

    EKS

--- main.tf
+++ main.tf
@@ -27,3 +27,21 @@ resource "aws_eks_node_group" "main" {
     max_unavailable = 1
   }
 }
+
+resource "aws_eks_access_entry" "workstation" {
+  cluster_name      = aws_eks_cluster.main.name
+  principal_arn     = "arn:aws:iam::667473681029:role/terraform"
+  # kubernetes_groups = ["group-1", "group-2"]
+  type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "workstation" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::667473681029:role/terraform"
+
+  access_scope {
+    type       = "Cluster"
+    # namespaces = ["example-namespace"]
+  }
+}
