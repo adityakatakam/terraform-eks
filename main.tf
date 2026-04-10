d59cf9578c47299f3c5abea213c6d43a1ff11782
akatakam <aditya.katakam@gmail.com>
2026-04-10 17:31:11 +0530

    EKS

--- main.tf
+++ main.tf
@@ -27,13 +27,6 @@ resource "aws_eks_node_group" "main" {
     max_unavailable = 1
   }
 }
-resource "null_resource" "kubeconfig" {
-  depends_on = [aws_eks_node_group.main]
-
-  provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name dev"
-  }
-}
 
 resource "aws_eks_access_entry" "workstation" {
   cluster_name      = aws_eks_cluster.main.name
